39f11046568b7f4ccb85d53be0c90f9d6e954e7a
unknown <kenichi@nakatani.jp>
2017-11-09 00:45:53 +0900

    modified train_dazai.sh to change rnn_size for training

--- ch5/train_dazai.sh
+++ ch5/train_dazai.sh
@@ -2,6 +2,6 @@
 
 python train.py --data_dir data/dazai \
     --checkpoint_dir model/dazai \
-    --rnn_size 1024 --gpu 0 --enable_checkpoint False
+    --rnn_size 128 --gpu 0 --enable_checkpoint False
 #    --checkpoint_dir /media/tajima/New\ Volume/cv/dazai \
 #    --init_from "$1"
